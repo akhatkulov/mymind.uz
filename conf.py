ad66b535cb564957800036e4b0cc14df36bdd1c1
Mekhroj Akhatkulov <151901779+akhatkulov@users.noreply.github.com>
2024-05-12 19:08:22 +0300

    building 1

---
+++ conf.py
@@ -0,0 +1,39 @@
+from flask import Flask, Blueprint, redirect, render_template, request, session
+from flask_login import LoginManager, UserMixin,login_user,login_required,logout_user,current_user
+from flask_wtf import FlaskForm
+from werkzeug.security import generate_password_hash, check_password_hash
+from wtforms import StringField, PasswordField, SubmitField
+from wtforms.validators import InputRequired, Length, ValidationError,DataRequired
+from flask_sqlalchemy import SQLAlchemy
+app = Flask(__name__)
+app.config['SECRET_KEY']="This is secret key, bro!"
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:1945@localhost/db'
+auth = Blueprint('auth',__name__,url_prefix="/auth")
+db = SQLAlchemy(app)
+
+login_manager = LoginManager()
+login_manager.init_app(app)
+login_manager.login_view = 'login'
+
+@login_manager.user_loader
+def load_user(user_id):
+    print(type(user_id))
+    return User.query.get(user_id)
+
+
+class User(db.Model,UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.Text(),nullable=False,unique=True, index=True)
+    password = db.Column(db.Text(),nullable=False)
+
+class Sign(FlaskForm):
+    username = StringField("Taxallusingiz",validators=[DataRequired()])
+    password = PasswordField("Parolingiz",validators=[DataRequired()])
+    submit = SubmitField("Tasdiqlash")
+
+class Login(FlaskForm):
+    username = StringField("Taxallusingiz",validators=[DataRequired()])
+    password = PasswordField("Parolingiz",validators=[DataRequired()])
+    submit =  SubmitField("Tasdiqlash")
+
+
---
+++ main.py
@@ -0,0 +1,68 @@
+from flask import Flask,redirect,render_template,Blueprint
+from conf import app,auth,db,generate_password_hash,Sign,Login,User,current_user,login_user,load_user,login_required,logout_user,check_password_hash
+
+with app.app_context():
+    db.create_all()
+# @app.route("/")
+# def home_page():
+#     return render_template('index.html')
+@app.route("/")
+def home():
+    if current_user.is_authenticated:
+        return f"Hey, {current_user.name}"
+    else:
+        return f"Hey Notanish Shoir"
+
+@auth.route("/signin")
+def signin():
+    form = Sign()
+    return render_template("sign.html",form=form)
+@auth.route("/signing",methods=['post'])
+def signing():
+
+        with app.app_context():
+            form = Sign()
+
+            print(form.username.data,form.password.data)
+            user = User(name=form.username.data,password=generate_password_hash(form.password.data))
+            db.session.add(user)
+            db.session.commit()
+            login_user(user,True)
+        return redirect("/dashboard")
+
+@auth.route("/signup")
+def signup():
+    form = Login()
+    return render_template("login.html",form=form)
+
+@app.route("/login",methods=['post'])
+def login_2():
+    form = Login()
+    print(form.username.data,form.password.data)
+    usr = User.query.filter_by(name=form.username.data).first()
+    if usr:
+        print(form.username.data,form.password.data)
+        if check_password_hash(usr.password,form.password.data):
+            login_user(usr)
+            return redirect("/dashboard")
+        else:
+            return "Parol Xato"
+    else:
+        return "Foydalanuvchi mavjud emas"
+@app.route("/profile/edit")
+def profile_edit():
+    return render_template("profile.html")    
+
+
+@app.route("/logout")
+@login_required
+def logout():
+    logout_user()
+    return "log out susses"
+@app.route("/dashboard")
+def dashboard():
+     print(current_user.name)
+     return str(current_user.name) 
+    #  return render_template("dashboard.html")
+app.register_blueprint(auth)
+app.run()
